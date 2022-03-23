03e3c2a6f9308ee43f587e31ca898c7cb81f99ad
Sam Barrett <samuelbarrett1234@btinternet.com>
2022-03-23 06:19:19 +0000

    Extended results aggregation output with options.

--- sam/results_agg.py
+++ sam/results_agg.py
@@ -8,14 +8,20 @@ import argparse as ap
 
 if __name__ == "__main__":
     parser = ap.ArgumentParser()
+    grp = parser.add_mutually_exclusive_group()
+    grp.add_argument("--filenames", action='store_true',
+                     help="Print only best model log filenames.")
+    grp.add_argument("--configs", action='store_true',
+                     help="Print only best model configurations.")
     parser.add_argument("log_filenames", type=str,
                         help="GLOB string to match all training logs.")
     args = parser.parse_args()
 
-    # mapping from tag to a 2-tuple containing
+    # mapping from tag to a 3-tuple containing
     # (i) the best validation performance seen so far,
     # and (ii) which configuration achieved it
-    best_for_tag = collections.defaultdict(lambda: (-math.inf, None))
+    # and (iii) the log filename of that configuration
+    best_for_tag = collections.defaultdict(lambda: (-math.inf, None, None))
 
     for fname in glob.glob(args.log_filenames, recursive=True):
         with open(fname, "r") as f:
@@ -26,11 +32,19 @@ if __name__ == "__main__":
                 # if the validation accuracy is better, save it
                 val_acc = float(row[3])
                 if val_acc > best_for_tag[cfg["tag"]][0]:
-                    best_for_tag[cfg["tag"]] = (val_acc, cfg)
+                    best_for_tag[cfg["tag"]] = (val_acc, cfg, fname)
 
-    print("The bests found were:")
-    for tag, acc_cfg in best_for_tag.items():
-        print("-----------------------")
-        print(f"Tag '{tag}' achieved validation accuracy {acc_cfg[0]} using configuration:")
-        pretty_cfg = json.dumps(acc_cfg[1], indent=4, sort_keys=True)
-        print(pretty_cfg)
+    if args.filenames:
+        for values in best_for_tag.values():
+            print(values[2])
+    elif args.configs:
+        for values in best_for_tag.values():
+            pretty_cfg = json.dumps(values[1], indent=4, sort_keys=True)
+            print(pretty_cfg)
+    else:
+        print("The bests found were:")
+        for tag, values in best_for_tag.items():
+            print("-----------------------")
+            print(f"Tag '{tag}' achieved validation accuracy {values[0]} using configuration:")
+            pretty_cfg = json.dumps(values[1], indent=4, sort_keys=True)
+            print(pretty_cfg)
